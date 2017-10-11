ccf11d6a5164883bde873e3e40a42c3add8f43bd
Aileen Wong <aileen.m.wong@gmail.com>
2017-10-11 13:55:06 -0400

    complete controller

---
+++ controllers/marker-controller.js
@@ -0,0 +1,78 @@
+const Marker = require('../models/markers');
+
+const markerController = {};
+
+//render all of the data from the db
+markerController.index = (req, res) => {
+  Marker.findAll()
+    .then(markers => {
+      res.render('#', {
+        data: markers,
+      })
+    }).catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+};
+
+//renders the data from a single item in the db
+markerController.show = (req, res) => {
+  Marker.findById(req.params.id)
+    .then(marker => {
+      res.render('#', {
+        data: marker,
+      });
+    }).catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+}
+
+//posts an item to the db
+markerController.create = (req, res) => {
+  Marker.create({
+      park_name: req.body.park_name,
+      address: req.body.address,
+      city: req.body.city,
+      state: req.body.state,
+      coordinates: req.body.coordinates,
+      image: req.body.image,
+      website: req.body.website,
+      description: req.body.description,
+      user_id: req.body.user_id
+    })
+    .then (grams => {
+      res.redirect('/');
+      })
+    .catch (err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+  };
+
+//this is the controller function that renders the data when searching for an item in the API
+markerController.search = (req, res) => {
+  Marker.search
+    .then(grams => {
+    res.render('#', {
+  })
+  .catch(err => {
+    res.status(500).json(err);
+  });
+  });
+}
+
+//this is the controller function that renders the data when deleting an item from the database
+markerController.delete = (req, res) => {
+  Marker.delete(req.params.id)
+  .then(() => {
+    res.redirect('#');
+    })
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  });
+};
+
+//export the controller
+module.exports = markerController;
---
+++ services/maps-service.js
@@ -0,0 +1,32 @@
+//require isomorphic-fetch
+require('isomorphic-fetch');
+//require dotenv to hide keys
+require('dotenv').config();
+
+//declare a function of getSearchResults with req, res, and next as parameters
+function getMapResults(req, res, next) {
+  //declare variables for search terms in the API key
+  const CLIENT_ID_MAP = process.env.SUPER_SECRET_CLIENT_ID_MAP;
+  const CLIENT_SECRET_MAP = process.env.SUPER_SECRET_CLIENT_SECRET_MAP;
+
+//   //fetch the api
+//   fetch(`https://api.foursquare.com/v2/venues/search?near=${req.body.locationSearch}+&query=${req.body.food}+&limit=10&client_id=${CLIENT_ID}&client_secret=${CLIENT_SECRET}&v=20170831`)
+//   //use then promise to fetch response
+//   .then((fetchRes) => {
+//     console.log('the data back from API call ->', fetchRes);
+//     //return the data
+//     return fetchRes.json();
+//     // use
+//   }).then((jsonFetchRes) => {
+//     console.log(jsonFetchRes);
+//     res.json(jsonFetchRes);
+//   }).catch((err) => {
+//     console.log(err);
+//     res.status(500).json({"data":"didn't find anything"})
+//   });
+}
+
+//export the helper file
+module.exports = {
+  getMapResults: getMapResults,
+}
