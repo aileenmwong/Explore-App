701ee8b38f5cb8cd9c457546d3b16c60f3f3c047
Aileen Wong <aileen.m.wong@gmail.com>
2017-10-13 21:03:02 -0400

    tried to merge changes with branch

--- db/migrations/migration.sql
+++ db/migrations/migration.sql
@@ -14,8 +14,11 @@ CREATE TABLE IF NOT EXISTS parks (
   city VARCHAR(255),
   state VARCHAR(255),
   coordinates VARCHAR(255),
+  lat INT,
+  lng INT,
   image VARCHAR(1000),
   website VARCHAR(1000),
   description VARCHAR(1000),
+  weather VARCHAR(1000),
   user_id INTEGER REFERENCES users
 );
