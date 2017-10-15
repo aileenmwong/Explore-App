36a53203fd0757ae2c6fa19654e21f5bf11de2d6
Aileen Wong <aileen.m.wong@gmail.com>
2017-10-15 13:09:13 -0400

    add styling to home page and single page

--- public/map.js
+++ public/map.js
@@ -1,6 +1,46 @@
 $(document).ready(function() {
 console.log('map.js is connected!');
+function initMap() {
 
+  // map options
+  var options = {
+    zoom: 5,
+    center: {lat: 39.8283, lng: -98.5795},
+  }
+  // New map
+  var map = new google.maps.Map(document.getElementById('map'), options);
+
+  //Listen for click on map
+  google.maps.event.addListener(map, 'click', function(event){
+    //add marker
+    addMarker({coords:event.latLng});
+  });
+
+//FOLLOWED THESE CLASS NOTES FROM ANOTHER WDI CLASS
+//https://wdi_sea.gitbooks.io/notes/content/05-express/additional-topics/express-geocode/readme.html
+
+  // for each marker passed through, add it to the map with a popup
+  markers.forEach(function(marker) {
+    console.log(marker);
+    var position = new google.maps.LatLng(marker.lat, marker.lng);
+    var googleMarker = new google.maps.Marker({
+      position: position,
+      title: marker.name,
+      map: map
+    })
+
+
+   // Bind a popup to the marker
+    googleMarker.addListener('click', function() {
+      var infoWindow = new google.maps.InfoWindow({
+        content: '<h3>' + marker.park_name + '</h3>'
+      });
+      infoWindow.open(map, googleMarker);
+    });
+      });
+
+}
+initMap();
 // RENDER MAP
 // Render maps tutorial: https://www.youtube.com/watch?v=Zxf1mnP5zcw
 
--- public/search.js
+++ public/search.js
@@ -14,7 +14,7 @@ let lng;
 
 var options = {
   zoom: 4,
-  center: {lat: 40.7134, lng: -74.0055},
+  center: {lat: 39.8283, lng: -98.5795},
 };
 // New map
 var map = new google.maps.Map(document.getElementById('searchMap'), options);
@@ -63,6 +63,7 @@ var map = new google.maps.Map(document.getElementById('searchMap'), options);
       console.log(cor.lat)
       console.log(typeof cor.lng)
       console.log(cor.lng)
+
       addMarkerFromApi(cor);
       };
 
@@ -141,10 +142,6 @@ var map = new google.maps.Map(document.getElementById('searchMap'), options);
 
   function addMarkerFromApi(cor) {
     console.log('inside add marker from api')
-  // cor.forEach(function addMarkerFromApi (cor) {
-    // console.log('this is cor -->', cor);
-    // console.log('this is cor lat -->', cor.lat);
-    // console.log('this is cor lat -->', cor.lng);
     let lat = cor.lat;
     let lng = cor.lng
     var position = new google.maps.LatLng(lat, lng);
@@ -155,23 +152,13 @@ var map = new google.maps.Map(document.getElementById('searchMap'), options);
       position: position,
       title: park_name,
       map: map
-    })
+    });
     console.log(googleMarker);
-
-    //   markers.forEach(function(marker) {
-    // console.log(marker);
-    // var position = new google.maps.LatLng(marker.lat, marker.lng);
-    // var googleMarker = new google.maps.Marker({
-    //   position: position,
-    //   title: marker.name,
-    //   map: map
-    // })
-
 
    // Bind a popup to the marker
     googleMarker.addListener('click', function() {
       var infoWindow = new google.maps.InfoWindow({
-        content: '<h3>' + park_name + '</h3>'
+        content: park_name
       });
       infoWindow.open(map, googleMarker);
     });
@@ -180,34 +167,14 @@ var map = new google.maps.Map(document.getElementById('searchMap'), options);
   // RENDER MAP
   // Render maps tutorial: https://www.youtube.com/watch?v=Zxf1mnP5zcw
   function initSearchMap() {
-    // map options
-
-
+    event.preventDefault();
     // Listen for click on map
     google.maps.event.addListener(map, 'click', function(event){
       // add marker
       addMarker({coords:event.latLng});
     });
 
-    // //array of markers
-    // var markers= [
-    // {
-    //   coords: {lat: 40.650002, lng: -73.949997},
-    //   //coords: `lat: ${latlong}, lng: ${variablename}`
-    //   content: '<h1>Brooklyn, NY</h1>'
-    // },
-    // {
-    //   coords: {lat: 32.8235, lng: -97.1706},
-    //   content: '<h1>Hurst, TX</h1>'
-    // }
-    // ];
-
-    // //loop through markers
-    // for(var i=0;i<markers.length;i++){
-    //   addMarker(markers[i]);
-    // };
-
-    // Add marker function
+    // Add marker function (based on click)
     function addMarker(props) {
       var marker = new google.maps.Marker({
         position: props.coords,
