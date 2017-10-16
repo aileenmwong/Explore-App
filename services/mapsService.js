6223eb62b4f00e032817bc235fcec27592df1b4b
Aileen Wong <aileen.m.wong@gmail.com>
2017-10-16 18:00:29 -0400

    commit

--- services/mapsService.js
+++ services/mapsService.js
@@ -1,61 +1,61 @@
-const util           = require('util');
-const fs             = require('fs');
-const request        = require('request-promise-native');
-const path           = require('path');
-const os             = require('os');
+// const util           = require('util');
+// const fs             = require('fs');
+// const request        = require('request-promise-native');
+// const path           = require('path');
+// const os             = require('os');
 
-const writeFile = util.promisify(fs.writeFile);
+// const writeFile = util.promisify(fs.writeFile);
 
-const tmpDir = fs.mkdtempSync(`${os.tmpdir()}${path.sep}`)
+// const tmpDir = fs.mkdtempSync(`${os.tmpdir()}${path.sep}`)
 
-async function getAllParks(state) {
-  try {
-    const { data: parks } = await request({
-      uri: 'https://developer.nps.gov/api/v0/parks',
-      qs: {
-        limit: 600,
-        stateCode: state
-      },
-      headers: {
-        'User-Agent': 'bloop',
-        authorization: 'B619A928-B3D8-4138-BDD4-B1C0CC6408C7',
-      },
-      json: true,
-    });
+// async function getAllParks(state) {
+//   try {
+//     const { data: parks } = await request({
+//       uri: 'https://developer.nps.gov/api/v0/parks',
+//       qs: {
+//         limit: 600,
+//         stateCode: state
+//       },
+//       headers: {
+//         'User-Agent': 'bloop',
+//         authorization: 'B619A928-B3D8-4138-BDD4-B1C0CC6408C7',
+//       },
+//       json: true,
+//     });
 
-    return parks;
+//     return parks;
 
-    // await writeFile(path.join(tmpDir, 'allParks.json'), JSON.stringify({ parks }));
-    // console.log('wrote file', path.join(tmpDir, 'allParks.json'));
-    // return parks;
-  } catch (e) {
-    console.log(e);
-  }
-}
+//     // await writeFile(path.join(tmpDir, 'allParks.json'), JSON.stringify({ parks }));
+//     // console.log('wrote file', path.join(tmpDir, 'allParks.json'));
+//     // return parks;
+//   } catch (e) {
+//     console.log(e);
+//   }
+// }
 
-function readParksFromFile() {
-  try {
-    console.log('READ file', path.join(tmpDir, 'allParks.json'))
-    return require(path.join(tmpDir, 'allParks.json')).parks;
-  } catch (e) {
-    return false;
-  }
-}
+// function readParksFromFile() {
+//   try {
+//     console.log('READ file', path.join(tmpDir, 'allParks.json'))
+//     return require(path.join(tmpDir, 'allParks.json')).parks;
+//   } catch (e) {
+//     return false;
+//   }
+// }
 
-async function getParks(state) {
-  // debugger;
-  let parks = await getAllParks(state);
-  console.log(parks)
-  return parks.map((park) => {
-    const [foo, lat, lng] =  park.latLong ? park.latLong.match(/^lat:(.+),.+:(.+)$/) : [0,0,0];
-    return {
-      ...park,
-      lat: parseFloat(lat),
-      lng: parseFloat(lng),
-    }
-  })
-};
+// async function getParks(state) {
+//   // debugger;
+//   let parks = await getAllParks(state);
+//   console.log(parks)
+//   return parks.map((park) => {
+//     const [foo, lat, lng] =  park.latLong ? park.latLong.match(/^lat:(.+),.+:(.+)$/) : [0,0,0];
+//     return {
+//       ...park,
+//       lat: parseFloat(lat),
+//       lng: parseFloat(lng),
+//     }
+//   })
+// };
 
-module.exports = {
-  getParks,
-}
+// module.exports = {
+//   getParks,
+// }
