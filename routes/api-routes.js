0cbeb5ee8774e3e6c68da681e22df92070375ee1
Aileen Wong <aileen.m.wong@gmail.com>
2017-10-17 16:13:07 -0400

    try to deploy again

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,12 +1,12 @@
-//require express
-const apiRouter = require('express').Router();
+// //require express
+// const apiRouter = require('express').Router();
 
-//require them middleware I created to grab data from api
-const npsService = require('../services/npsService');
+// //require them middleware I created to grab data from api
+// const npsService = require('../services/npsService');
 
-//create a route to post the data
-apiRouter.post('/', npsService.getNpsResults)
-apiRouter.get('/', npsService.getNpsResults)
+// //create a route to post the data
+// apiRouter.post('/', npsService.getNpsResults)
+// apiRouter.get('/', npsService.getNpsResults)
 
-//export the router
-module.exports = apiRouter;
+// //export the router
+// module.exports = apiRouter;
